b2055716002efda7d828632ccd20095955105484
Andy Chen <acchen@cpp.edu>
2024-04-04 22:42:58 -0700

    added write handler to ppu scroll port

--- src/ppu.c
+++ src/ppu.c
@@ -4,9 +4,7 @@
 #include "../includes/ppu.h"
 #include "../includes/cartridge.h"
 #include "../includes/cpu.h"
-
-#define PPU_RAM_SIZE 1024 * 2
-#define PALETTE_SIZE 32
+#include "../includes/bus.h"
 
 // cpu mapped addresses of PPU ports at 0x2000 - 0x2007 and 0x4041
 
@@ -22,41 +20,61 @@
 
 #define PALETTE_START 0x3F00
 
-// ppu registers
-
-static uint8_t ppu_control;
-static uint8_t ppu_mask;
-static uint8_t ppu_status;
-static uint8_t oam_address;
-static uint8_t oam_data;
-static uint8_t ppu_scroll;
-static uint8_t oam_dma;
-
-static uint16_t vram_address; // effective 16-bit vram address
-static uint8_t vram_hi_address; // lo and hi addresses used to form the full 16-bit effective vram address
-static uint8_t vram_lo_address;
-static uint8_t vram_buffer; // internal buffer that holds contents being read from vram
-
+static uint8_t ppu_control = 0;
+static uint8_t ppu_mask = 0;
+static uint8_t ppu_status = 0xA0;
+static uint8_t oam_address = 0;
+static uint8_t oam_data = 0;
 
 static bool write_toggle = false; // false: first write, true: second write for PPUADDR and PPUSCROLL port, set to false when ppu_status is read
-static uint8_t open_bus;
+static uint8_t x_register = 0; // fine x scroll (3 bits)
+static uint16_t t_register = 0; // 15-bit temporary vram address
+static uint16_t v_register = 0; // current vram address that when used through port $2007 to access ppu memory only 14 bits are used, but it is otherwise a 15 bit register
+
+static uint8_t vram_hi_address = 0; // lo and hi addresses used to form the full effective vram address
+static uint8_t vram_lo_address = 0;
+static uint8_t vram_buffer = 0; // internal buffer that holds contents being read from vram
+
+static uint8_t open_bus = 0;
 
 // memory
 
+#define PPU_RAM_SIZE 1024 * 2
+#define PALETTE_SIZE 32
+#define OAM_SIZE 256
+
 static uint8_t vram[PPU_RAM_SIZE];
 static uint8_t palette_ram[PALETTE_SIZE];
+static uint8_t oam_ram[OAM_SIZE];
+
+static uint16_t scanline = 261;
+static uint16_t cycle = 0;
 
 void ppu_cycle(void)
 {
-   
+   // scanline 0-239 (i.e 240 scanlines) are the visible scanlines to the display
+   if (scanline >= 0 && scanline <= 239)
+   {
+      
+
+   }
+   else if (scanline == 261) // pre-render scanline
+   {
+
+   }
 }
 
 void ppu_cpu_write(uint16_t position, uint8_t data)
 {
    switch(position)
    {
-      case PPUCTRL: // write only
+      case PPUCTRL:
          ppu_control = data;
+         
+         // transfer bits 0-1 of ppu_control to bits 10-11 of t_register
+         uint16_t NN = (ppu_control & 0x3) << 10;
+         t_register = t_register & 0xF3FF; // clear bits 10-11 of t_register before transfering bits 0-1
+         t_register = t_register | NN;
          break;
       case PPUMASK:
          ppu_mask = data;
@@ -65,44 +83,63 @@ void ppu_cpu_write(uint16_t position, uint8_t data)
          oam_address = data;
          break;
       case PPUSCROLL:
-         ppu_scroll = data;
-        // write_toggle = !write_toggle;
+         if (!write_toggle) // first write
+         {
+            x_register = data & 0x7; // bits 0-2 stored into x_register
+
+            t_register = t_register & 0x7FE0; // clear bits 0-4 before transfer
+            t_register = t_register | ( (data & 0xF8) >> 3 ); // bits 3-7 stored into bits 0-4 of t_register
+         }
+         else // second write
+         {
+            t_register = t_register & 0x0C1F; // clear bits 5-9 and bits 12-14 before transfer
+            t_register = t_register | ( (data & 0x7) << 12 ); // bits 0-2 stored into bits 12-14 of t_register
+            t_register = t_register | ( (data & 0xF8) << 2 ); // bits 3-7 stored into bits 5-9 of t_register
+         }
+
+         write_toggle = !write_toggle;
          break;
       case PPUADDR:
          if (!write_toggle)
          {
             // writing high byte (first write)
-            vram_hi_address = data & 0x3F; // only get 6 high bits because ppu only addresses 0x0000 to 0x3FFF (6 hi bits + 8 low bits = 14 bits)
+            vram_hi_address = data;
          }
          else
          {
             // writing low byte (second write)
             vram_lo_address = data;
-            vram_address = (vram_hi_address << 8) | vram_lo_address;
+            v_register = ( (vram_hi_address << 8) | vram_lo_address ) & 0x3FFF;
          }
 
          write_toggle = !write_toggle;
          break;
-      case OAMDMA:
-         oam_dma = data;
+      case OAMDMA: // todo: handle clock cycles
+         uint16_t read_address = data << 8;
+         for (size_t i = 0; i < 256; ++i)
+         {
+            oam_data = cpu_bus_read(read_address | i);
+            oam_ram[oam_address] = oam_data;
+            oam_address += 1;
+         }
          break;
-      case OAMDATA: // read/write
+      case OAMDATA:
          oam_data = data;
+         oam_ram[oam_address] = oam_data;
+         oam_address += 1;
          break;
       case PPUDATA:
-         if ( vram_address >= PALETTE_START )
+         if ( (v_register & 0x3FFF) >= PALETTE_START )
          {
             // writing to palette ram
-            palette_ram[vram_address] = data;
+            palette_ram[v_register & 0x1F] = data;
          }
          else
          {
-            cartridge_ppu_write(vram_address, data, vram);
+            cartridge_ppu_write(v_register & 0x3FFF, data, vram);
          }
          
-         vram_address += (ppu_control & 4) ? 32 : 1; // increment vram address by 1 if bit 2 of control register is 0, else increment by 32
-         break;
-      case PPUSTATUS: // read only
+         v_register = ( v_register + ((ppu_control & 4) ? 32 : 1) ) & 0x7FFF; // increment vram address by 1 if bit 2 of control register is 0, else increment by 32
          break;
    }
 
@@ -113,27 +150,21 @@ uint8_t ppu_cpu_read(uint16_t position)
 {
    switch(position)
    {
-      case PPUCTRL: // write only
-      case PPUMASK:
-      case OAMADDR:
-      case PPUSCROLL:
-      case PPUADDR:
-      case OAMDMA:
-         break;
       case OAMDATA: // read/write
+         oam_data = oam_ram[oam_address];
          open_bus = oam_data;
          break;
       case PPUDATA:
          open_bus = vram_buffer;
-         vram_buffer = cartridge_ppu_read(vram_address, vram);
+         vram_buffer = cartridge_ppu_read(v_register & 0x3FFF, vram);
          
          // when reading palette, data is returned directly from palette ram rather than the internal read buffer
-         if ( vram_address >= PALETTE_START )
+         if ( v_register & 0x3FFF >= PALETTE_START )
          {
-            return palette_ram[ vram_address & 0x1F ];
+            return palette_ram[v_register & 0x1F];
          }
 
-         vram_address += (ppu_control & 4) ? 32 : 1; // increment vram address by 1 if bit 2 of control register is 0, else increment by 32
+         v_register += (ppu_control & 4) ? 32 : 1; // increment vram address by 1 if bit 2 of control register is clear, else increment by 32
          break;
       case PPUSTATUS: // read only
          open_bus = (ppu_status & 0xE0) | open_bus;
